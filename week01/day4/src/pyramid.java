6886807bea492b1c201a5759369cb8c9a21f3953
Kálló <kalloaa9@gmail.com>
2019-09-12 17:05:16 +0200

    added pyramid

---
+++ week01/day4/src/pyramid.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class pyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("set how high you want the pyramid to be");
+        int pyramid = scanner.nextInt();
+        int pyraneg = (pyramid*-1);
+        int pyrapoz = (pyramid);
+        for(int i =0; i<=pyramid;i++){
+            System.out.println();
+            for (int j =pyraneg; j<=pyrapoz;j++){
+                if (j>=(i*-1) && j<=0){
+                    System.out.print("+");
+                }
+                else if (j<=i && j >=0){
+                    System.out.print("+");
+                }
+                else{
+                    System.out.print("-");
+                }
+            }
+        }
+
+    }
+}
