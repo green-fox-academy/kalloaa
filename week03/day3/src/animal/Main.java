cfa9b79d745e6abc9e2f132b1ee36726323dcb34
Kálló <kalloaa9@gmail.com>
2019-09-25 14:37:49 +0200

    added sharpie

--- week03/day3/src/animal/Main.java
+++ week03/day3/src/animal/Main.java
@@ -1,4 +1,10 @@
 package animal;
 
 public class Main {
+    public static void main(String[] args) {
+        Animal dog = new Animal();
+        dog.drink();
+        dog.eat();
+        dog.play();
+    }
 }
---
+++ week03/day3/src/sharpie/main.java
@@ -0,0 +1,9 @@
+package sharpie;
+
+public class main {
+    public static void main(String[] args) {
+        Sharpie sharpie = new Sharpie("Black", 10);
+        sharpie.use();
+
+    }
+}
