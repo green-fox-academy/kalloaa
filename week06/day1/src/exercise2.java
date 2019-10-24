7cf9b4c957b3a905f159510d3c74ba1ca45541f2
Kálló <kalloaa9@gmail.com>
2019-10-24 11:33:41 +0200

    added exercise 2

---
+++ week06/day1/src/exercise2.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class exercise2 {
+  List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+  List<Integer> positives = numbers.stream()
+      .filter(s->s>0)
+      .collect(Collectors.toList());
+
+}
