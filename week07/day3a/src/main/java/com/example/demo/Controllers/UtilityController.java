0b08697a5d04e6f68879d8ec3bfc08123685f0f7
Kálló <kalloaa9@gmail.com>
2019-10-31 14:31:56 +0100

    added ceasar

--- week07/day3a/src/main/java/com/example/demo/Controllers/UtilityController.java
+++ week07/day3a/src/main/java/com/example/demo/Controllers/UtilityController.java
@@ -1,14 +1,23 @@
 package com.example.demo.Controllers;
 
 import com.example.demo.Services.UtilityService;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UtilityController {
+  private List<String> savedCeasar = new ArrayList<>();
+
+  public List<String> getSavedCeasar() {
+    return savedCeasar;
+  }
+
 
   private UtilityService utilityService;
 
@@ -18,7 +27,8 @@ public class UtilityController {
   }
 
   @GetMapping(value = "/useful")
-  public String showIndex(){
+  public String showIndex(Model model){
+    model.addAttribute("ceasars",savedCeasar);
     return "index";
   }
 
@@ -27,10 +37,22 @@ public class UtilityController {
     model.addAttribute("randomcolor",utilityService.randomColor());
     return "RC";
   }
+
   @GetMapping(value = "/useful/email")
   public String showEV(Model model, @RequestParam String email){
     System.out.println(email);
     model.addAttribute("isValid",utilityService.validateEmail(email));
     return "EV";
+  }
+
+  @GetMapping(value = "/useful/ceasar")
+  public String showCEAS(){
+    return "CEAS";
+  }
+
+  @PostMapping(path = "/useful/ceasar/add")
+  public String ceasarIt(@RequestParam String text,@RequestParam int number){
+    savedCeasar.add(utilityService.caesar(text,number));
+    return "redirect:/useful";
   }
 }
